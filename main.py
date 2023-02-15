9b1fec9fcb85f06a4d89d0fab7fba618c5c8e61c
esp3j0 <esp3j0@outlook.com>
2023-02-15 05:42:53 -0800

    update .gitignore

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from os import popen
 from time import sleep
 from os import system,path
 import threading
+import concurrent.futures
+MAX_THREADS = 10
 
 
 class pdb_proteintoolweb:
@@ -49,7 +51,7 @@ class pdb_proteintoolweb:
 class Contact_maps_proteintoolsweb:
     sum_csv_len = 0
     pdb_list = []
-    hub = 'http://kali.lan:4444/wd/hub'
+    hub = 'http://localhost:4444/wd/hub'
     contact_map_url = "https://proteintools.uni-bayreuth.de/contacts/"
     pdb_upload_floder = './pdbs/{}'
     csv_save_floder = "./csvs/"
@@ -73,23 +75,28 @@ class Contact_maps_proteintoolsweb:
         return driver
     def wait_downlaod(self,name):
         timeout = 0
-        mx_timeout = 20
+        mx_timeout = 10
         while(True):
             timeout += 1
             if timeout > mx_timeout:
+                system("echo "+name.replace(".pdb","")+">> failednamelist.txt")
+                system("rm -rf ./csvs/{}".format(name.replace(".pdb","")))
                 return False
-            if path.exists("./csvs/{}/".format(name)):
+            if path.exists("./csvs/{}/contact_maps_table.csv".format(name.replace(".pdb",""))):
                 break
             else:
-                sleep(1)
+                sleep(5)
         while(True):
             timeout += 1
             if timeout > mx_timeout:
                 return False
-            presize = path.getsize("./csvs/{}/contact_maps_table.csv".format(name))
-            sleep(1)
-            nowsize = path.getsize("./csvs/{}/contact_maps_table.csv".format(name))
-            if(presize == nowsize):
+            presize = path.getsize("./csvs/{}/contact_maps_table.csv".format(name.replace(".pdb","")))
+            sleep(15)
+            nowsize = path.getsize("./csvs/{}/contact_maps_table.csv".format(name.replace(".pdb","")))
+            print(name+"presize is :"+str(presize))
+            print(name+"nowsize is :"+str(nowsize))
+            if(nowsize!=0 and presize == nowsize):
+                sleep(40)
                 return True
     
     def get_contact_map(self,name):
@@ -98,22 +105,23 @@ class Contact_maps_proteintoolsweb:
         try:
             print(name)
             driver.get(self.contact_map_url)
-            upload = driver.find_element_by_id("customFile")
+            upload = driver.find_element(By.ID,"customFile")
             upload.send_keys(self.pdb_upload_floder.format(name))
-            submit = driver.find_element_by_id("btnFetch")
+            submit = driver.find_element(By.ID,"btnFetch")
             submit.click()
-            # WebDriverWait(driver, 360, 1).until(
-            #         EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/a"))
-            #     )
-            download = driver.find_element_by_xpath("/html/body/div[1]/div[2]/a")
+            wait = WebDriverWait(driver, 70)
+            download = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/a")
             download.click()
             if(self.wait_downlaod(name)):
+            #if(WebDriverWait(driver, 60).until(lambda driver: os.path.exists('/path/to/download/file') and os.path.getsize('/path/to/download/file') > 0)):
                 self.sum_csv_len = self.sum_csv_len - 1
                 print(name+"已完成,剩余"+str(self.sum_csv_len)+"个")
                 driver.quit()
             else:
                 print(name+"下载失败,line 126")
-        except:
+                driver.quit()
+        except Exception as ex:
+            print("出现异常%s"%ex)
             print("except"+name)
             driver.quit()
     
@@ -121,16 +129,33 @@ class Contact_maps_proteintoolsweb:
     
     def csv_thread(self):
         get_csvs_thread_list = []
-        for name in self.pdb_list:
-            self.sum_csv_len = self.sum_csv_len + 1
-            t = threading.Thread(target=self.get_contact_map,args=(name,))
-            get_csvs_thread_list.append(t)
-        for t in get_csvs_thread_list:
-            sleep(5)
-            t.start()
-        for t in get_csvs_thread_list:
-            t.join()
+        max_thread = 10
+        now_thread = 0
+        with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            # 创建线程池
+            futures = []
+            for name in self.pdb_list:
+                future = executor.submit(self.get_contact_map, name)
+                futures.append(future)
+
+            # 等待所有任务完成
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    result = future.result()
+                    # 处理任务结果
+                except Exception as exc:
+                    print('任务失败：%s' % exc)
+        # for name in self.pdb_list:
+        #     self.sum_csv_len = self.sum_csv_len + 1
+        #     t = threading.Thread(target=self.get_contact_map,args=(name,))
+        #     get_csvs_thread_list.append(t)
+        # for t in get_csvs_thread_list:
+        #     sleep(3)
+        #     t.start()
+        # for t in get_csvs_thread_list:
+        #     t.join()
     def clean(self):
+        system("rm -rf ./pre_csvs/*")
         system("mv ./csvs/* ./pre_csvs/")
     def __init__(self) -> None:
         self.clean()
